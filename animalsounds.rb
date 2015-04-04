1e9efa5b44d357204026fc076ea96ac182c1cf5f
judemolke <jude@generalassemb.ly>
2015-04-04 09:48:38 -0700

    animalsounds.rb

---
+++ animalsounds.rb
@@ -0,0 +1,13 @@
+puts "What is your favorie animal?"
+
+UserInput1 = gets.chomp
+
+puts "What sound does it make?"
+
+UserInput2 = gets.chomp + " "
+
+puts "the " + UserInput1 + " goes " + UserInput2 * 3
+
+
+
+
