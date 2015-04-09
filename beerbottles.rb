a0cfc2f962383ef05dbf594aa32f5e69dce93d46
judemolke <jude@generalassemb.ly>
2015-04-09 13:19:59 -0700

    Add beerbottles.rb homework

---
+++ beerbottles.rb
@@ -0,0 +1,11 @@
+beer_num = 99
+
+while beer_num > 0 do
+	puts "#{beer_num} bottles of beer on the wall, #{beer_num} bottles of beer"
+
+		beer_num -= 1
+
+	puts "Take one down and pass it around, #{beer_num} bottles of beer on the wall."
+end
+
+
